4aacd4d9c8b5fac64826ffe0a3e8992566d88991
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-02-19 18:50:06 +0900

    first commit

---
+++ src/test/kotlin/com/example/ktknowledgeTodo/KtknowledgeTodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktknowledgeTodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtknowledgeTodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
