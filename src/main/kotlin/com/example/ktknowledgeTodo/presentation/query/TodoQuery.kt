df00292ba637b1c1695fc36eb89f8248d9ed3017
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-02-20 20:24:14 +0900

    add todo to TodoQuery

--- src/main/kotlin/com/example/ktknowledgeTodo/presentation/query/TodoQuery.kt
+++ src/main/kotlin/com/example/ktknowledgeTodo/presentation/query/TodoQuery.kt
@@ -1,15 +1,27 @@
 package com.example.ktknowledgeTodo.presentation.query
 
 import com.example.ktknowledgeTodo.presentation.type.TodoType
+import com.example.ktknowledgeTodo.presentation.util.toInt
+import com.example.ktknowledgeTodo.usecase.FindTodoByIdUseCase
 import com.example.ktknowledgeTodo.usecase.FindTodosUseCase
+import com.expediagroup.graphql.generator.scalars.ID
 import com.expediagroup.graphql.server.operations.Query
 import org.springframework.stereotype.Component
 
 @Component
-class TodoQuery(private val findTodosUseCase: FindTodosUseCase) : Query {
+class TodoQuery(
+    private val findTodosUseCase: FindTodosUseCase,
+    private val findTodoByIdUseCase: FindTodoByIdUseCase
+) : Query {
     fun todos(): List<TodoType> {
         val todoDtos = findTodosUseCase.call()
 
         return todoDtos.map { TodoType(it) }
+    }
+
+    fun todo(id: ID): TodoType? {
+        val todoDto = findTodoByIdUseCase.call(id.toInt())
+
+        return todoDto?.let { TodoType(it) }
     }
 }
