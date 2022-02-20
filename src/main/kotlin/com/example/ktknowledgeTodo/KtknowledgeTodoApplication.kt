f102788cca6ba97e91ec91c3f6dfb1b0561ce340
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-02-20 16:18:54 +0900

    add CustomSchemaGeneratorHooks

--- src/main/kotlin/com/example/ktknowledgeTodo/KtknowledgeTodoApplication.kt
+++ src/main/kotlin/com/example/ktknowledgeTodo/KtknowledgeTodoApplication.kt
@@ -1,10 +1,15 @@
 package com.example.ktknowledgeTodo
 
+import com.example.ktknowledgeTodo.presentation.hooks.CustomSchemaGeneratorHooks
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class KtknowledgeTodoApplication
+class KtknowledgeTodoApplication {
+    @Bean
+    fun customSchemaGeneratorHooks() = CustomSchemaGeneratorHooks()
+}
 
 fun main(vararg args: String) {
     runApplication<KtknowledgeTodoApplication>(*args)
